900db999ec1bd26013db85ffbddcb7162bd2268b
Carter Fulcher <carter@fulchereng.com>
2020-07-31 10:52:38 -0600

    added exports&pages

---
+++ exports.js
@@ -0,0 +1,31 @@
+const directory = [
+    {
+        name: 'Bot Building',
+        key: 'sub1',
+        pages: [
+            {
+                name: 'Getting Started',
+                key: '1',
+                filepath: 'pages/gettingstarted.md'
+            },
+            {
+                name: 'Building Bots',
+                key: '2',
+                filepath: 'pages/bots.md'
+            }
+        ]
+    },
+    {
+        name: 'Backtesting',
+        key: 'sub2',
+        pages: [
+            {
+                name: 'Basic',
+                key: '3',
+                filepath: 'pages/backtesting.md'
+            }
+        ]
+    }
+];
+
+export default directory;
